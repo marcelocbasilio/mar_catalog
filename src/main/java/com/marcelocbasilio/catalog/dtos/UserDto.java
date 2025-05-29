2fde4f2ac6d82319afe6be04cf335372dcceb3f1
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-05-29 02:37:46 -0300

    Created User repository and Dto.

---
+++ src/main/java/com/marcelocbasilio/catalog/dtos/UserDto.java
@@ -0,0 +1,77 @@
+package com.marcelocbasilio.catalog.dtos;
+
+import com.marcelocbasilio.catalog.entities.User;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserDto implements Serializable {
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+
+    public UserDto(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto entity = (UserDto) o;
+        return Objects.equals(this.id, entity.id) &&
+                Objects.equals(this.firstName, entity.firstName) &&
+                Objects.equals(this.lastName, entity.lastName) &&
+                Objects.equals(this.email, entity.email) &&
+                Objects.equals(this.password, entity.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "firstName = " + firstName + ", " +
+                "lastName = " + lastName + ", " +
+                "email = " + email + ", " +
+                "password = " + password + ")";
+    }
+}
